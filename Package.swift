e040d71e9bb8e1b37c193508e366b54d9e7f631c
richardjorne <154006102+richardjorne@users.noreply.github.com>
2025-08-24 11:37:27 -0400

    add watchos support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftRipple",
-    platforms: [.iOS(.v13),.macOS(.v10_15),.tvOS(.v13),.visionOS(.v1)],
+    platforms: [.iOS(.v13),.macOS(.v10_15),.tvOS(.v13),.visionOS(.v1), .watchOS(.v6)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
